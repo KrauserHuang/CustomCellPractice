0f8a8fc9c3632a1b40ca52bacc1e6c31df6fa916
krauserhuang <crescentdence@gmail.com>
2021-10-17 09:12:51 +0800

    Initial Commit

---
+++ CustomCellPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellPractice
+//
+//  Created by Tai Chin Huang on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
